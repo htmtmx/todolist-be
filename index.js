710791eac29c4a4e6657bd38272ac0af2350874c
htmtmx <cesar.hpp96@hotmail.com>
2024-09-12 03:51:04 -0600

    🎉 Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const {dbConnection} = require('./db/config');
+
+const app = express();
+dbConnection();
+
+app.use(cors());
+
+// Directorio Público
+app.use(express.static('public'));
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Rutas
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/todos', require('./routes/todos'));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
